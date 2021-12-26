34fba8700ec543747bfb505144a4932546b0466f
Jeremy Stanley <jeremy@instructure.com>
2021-12-26 13:43:05 -0700

    1

---
+++ 1.rs
@@ -0,0 +1,17 @@
+use std::io::BufRead;
+
+fn read_lines<F>(mut f: F) where F: FnMut(String) {
+    let args: Vec<String> = std::env::args().collect();
+    let file = std::fs::File::open(&args[1]).expect("failed to open input");
+    for lr in std::io::BufReader::new(&file).lines() {
+        f(lr.expect("failed to read line"));
+    }
+}
+
+fn main() {
+    let mut sum = 0;
+    read_lines(|line| {
+        sum += line.parse::<i64>().expect("failed to parse number");
+    });
+    println!("{}", sum);
+}
