2926f2cad5e5f17e67f6c545c87c308f3c85ef3b
Jeremy Stanley <jeremy@instructure.com>
2021-12-26 21:23:14 -0700

    2

---
+++ 2.rs
@@ -0,0 +1,28 @@
+use std::io::BufRead;
+use std::collections::HashMap;
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let file = std::fs::File::open(&args[1]).unwrap();
+    let mut twos = 0;
+    let mut threes = 0;
+    for line in std::io::BufReader::new(&file).lines() {
+        let mut counts = HashMap::new();
+
+        for char in line.unwrap().chars() {
+            if let Some(count) = counts.get_mut(&char) {
+                *count += 1;
+            } else {
+                counts.insert(char, 1);
+            }
+        }
+
+        if counts.values().any(|s| s == &2) {
+            twos += 1;
+        }
+        if counts.values().any(|s| s == &3) {
+            threes += 1;
+        }
+    }
+    println!("{}", twos * threes);
+}
---
+++ 2b.rs
@@ -0,0 +1,24 @@
+use std::io::BufRead;
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let file = std::fs::File::open(&args[1]).unwrap();
+    let words: Vec<String> = std::io::BufReader::new(&file).lines().map(|line| line.unwrap()).collect();
+
+    for w0 in &words {
+        for w1 in &words {
+            if w0.len() == w1.len() {
+                let mut common_letters = String::new();
+                for pair in w0.chars().zip(w1.chars()) {
+                    if pair.0 == pair.1 {
+                        common_letters.push(pair.0);
+                    }
+                }
+                if common_letters.len() == w0.len() - 1 {
+                    println!("{}", common_letters);
+                    return;
+                }
+            }
+        }
+    }
+}
